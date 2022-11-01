f1a042e07ab4008c6d043695a8633966fda4f7bb
Taras Savchyn <savik1992@gmail.com>
2022-11-01 18:57:57 +0200

    start HW_8

---
+++ HW_8/main.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from datetime import date
+
+from classes import Employee
+from decorator import timer
+
+employees = [
+    Employee(name="John", start=date(2022, 5, 20), rate=Decimal("11"), taxes=10),
+    Employee(name="Alex", start=date(2022, 5, 1), rate=Decimal("50"), taxes=20),
+    Employee(name="Kseniya", start=date(2022, 5, 10), rate=Decimal("50"), taxes=40),
+    Employee(name="Valeriy", start=date(2022, 4, 20), rate=Decimal("60"), taxes=50),
+    Employee(name="Vera", start=date(2019, 5, 20), rate=Decimal("99"), taxes=60),
+    Employee(name="Suzanna", start=date(2021, 5, 20), rate=Decimal("20"), taxes=10),
+    Employee(name="Gleb", start=date(2021, 5, 20), rate=Decimal("20"), taxes=10),
+]
+
+
+@timer
+def show_table():
+    ...
+
+
+def update_table():
+    ...
+
+
+if __name__ == "__main__":
+    show_table()
+    update_table()
+    show_table()
