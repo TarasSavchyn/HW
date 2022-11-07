e46a8d79e8b38a27537aaf7d358aac79f4cd12df
Taras Savchyn <savik1992@gmail.com>
2022-11-07 21:58:27 +0200

    add HW_10

---
+++ HW_10/app/main.py
@@ -0,0 +1,88 @@
+import logging
+from models.models import Plant, Employee
+
+date_strftime_format = "%d-%b-%y %H:%M:%S"
+message_format = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(filename="logs/main.log", format= message_format, datefmt= date_strftime_format, encoding="UTF-8", level=logging.DEBUG)
+
+
+while True:
+    print("1. Add new plant \n"
+          "2. Get all plants\n"
+          "3. Get plant by id\n"
+          "4. Delete plant by id\n"
+          "5. Add new employee\n"
+          "6. Get all employee\n"
+          "7. Get employee by id\n"
+          "8. Delete employee by id\n"
+          )
+    logging.info("Menu printed!!!")
+    try:
+        flag = int(input("Choose: "))
+    except ValueError as e:
+        logging.error(e)
+        print("You must to type a number!!!")
+        continue
+
+    if flag == 1:
+        name = input("Type name of new plant: ")
+        location = input("Type location of plant: ")
+        plant = Plant(name, location)
+        plant.save()
+    elif flag == 2:
+        plants = Plant.get_all()
+        for plant in plants:
+            print(plant["id"])
+            print(plant["name"])
+            print(plant["location"])
+    elif flag == 3:
+        try:
+            id = int(input('Type id of plant: '))
+        except ValueError as e:
+            logging.error(e)
+            continue
+        plant = Plant.get_by_id(id)
+        print(plant["id"])
+        print(plant["name"])
+        print(plant["location"])
+    elif flag == 4:
+        id = int(input('Type id of plant which you want to delete: '))
+        Plant.delete(id)
+    elif flag == 5:
+        name = input("Type name of employee: ")
+        email = input("Type email of employee: ")
+        plant_id = int(input("Type id of plant: "))
+        employee = Employee(name, email, plant_id)
+        try:
+            employee.save()
+        except ValueError as e:
+            logging.error(e)
+            print("You need to write a plant_id of existing plant!")
+
+    elif flag == 6:
+        employees = Employee.get_all()
+        for employee in employees:
+            print(employee["id"])
+            print(employee["name"])
+            print(employee["email"])
+    elif flag == 7:
+        try:
+            id = int(input('Type id of employee: '))
+        except ValueError:
+            logging.error("Id of employee not a number")
+            continue
+        employee = Employee.get_by_id(id)
+        print(employee["id"])
+        print(employee["name"])
+        print(employee["email"])
+    elif flag == 8:
+        id = int(input('Type id of employee: '))
+        Employee.delete(id)
+
+
+
+
+
+# file = open("test.txt", "a")
+# file.write("a")
+# file.close()
